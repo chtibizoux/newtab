de64e29f4c09414198276c08ae8a416326fb1dea
Clément <45465110+chtibizoux@users.noreply.github.com>
2022-07-22 14:15:17 +0200

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//     if (request.command == "get") {
-//         fetch(request.url)
-//             .then(response => response.text())
-//             .then(response => sendResponse(response))
-//             .catch(e => sendResponse("Error: " + e))
-//         return true;
-//     }
-// });
